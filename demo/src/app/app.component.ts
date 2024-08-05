a3770d62f0c48021a5b4d6544c654f5253b18e12
yueyingliang <liang.yuey@northeastern.edu>
2024-08-05 11:38:05 -0400

    coupon

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CouponComponent } from './coupon/coupon.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CouponComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
